07b8d569769f31bcc6630e2a36d21931bda6f721
ShepelevaRT <rt.shepeleva@gmail.com>
2024-07-31 14:23:00 +0300

    Добавила класс Triangle. Тесты пока не неписаны

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -2,6 +2,7 @@ package ru.stqa.geometry;
 
 import ru.stqa.geometry.figures.Rectangle;
 import ru.stqa.geometry.figures.Square;
+import ru.stqa.geometry.figures.Triangle;
 
 public class Geometry {
     public static void main(String[] args) {
@@ -11,6 +12,10 @@ public class Geometry {
 
         Rectangle.printRectangleArea(3.0, 5.0);
         Rectangle.printRectangleArea(9.0, 3.0);
+
+        Triangle.printTrianglePerimeter(10.0, 10.0, 12.0);
+        Triangle.printTriangleArea(10.0, 10.0, 12.0);
+
     }
 
 }
---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -0,0 +1,24 @@
+package ru.stqa.geometry.figures;
+
+import static java.lang.Math.sqrt;
+
+public class Triangle {
+    public static void printTrianglePerimeter(double a, double b, double c) {
+        String text = String.format("Периметр треугольника со сторонами %f, %f и %f = %f", a,b,c, trianglePerimeter(a,b,c));
+        System.out.println(text);
+    }
+
+    private static double trianglePerimeter(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    public static void printTriangleArea(double a, double b, double c) {
+        String text = String.format("Площадь треугольника со сторонами %f, %f и %f = %f", a,b,c, triangleArea(a,b,c));
+        System.out.println(text);
+    }
+
+    private static double triangleArea(double a, double b, double c) {
+        var p = trianglePerimeter(a,b,c);
+        return sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
