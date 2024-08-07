09c299402a1f261966b7a401fe266edc9fdfe85c
ShepelevaRT <rt.shepeleva@gmail.com>
2024-08-07 18:03:42 +0300

    Задание №4: Потренироваться использовать сравнение объектов

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -16,4 +16,19 @@ public record Rectangle(double a, double b) {
         String text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", s.a, s.b, s.rectangleArea());
         System.out.println(text);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Rectangle rectangle = (Rectangle) o;
+        return ((Double.compare(this.a, rectangle.a) == 0) && (Double.compare(this.b, rectangle.b) == 0))
+                || ((Double.compare(this.b, rectangle.a) == 0) && (Double.compare(this.a, rectangle.b) == 0));
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -31,4 +31,46 @@ public class TriangleTests {
         }
     }
 
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(5.0, 4.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality2() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(5.0,3.0, 4.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality3() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(4.0,5.0, 3.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality4() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(4.0,3.0, 5.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality5() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(3.0,5.0, 4.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
+    @Test
+    void testEquality6() {
+        var t1 = new Triangle(5.0, 4.0, 3.0);
+        var t2 = new Triangle(3.0,4.0, 5.0);
+        Assertions.assertEquals(t1, t2);
+    }
+
 }
