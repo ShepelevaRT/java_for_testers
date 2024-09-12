56ff35c8fc924a043a65ae5546e413cbbef727d3
ShepelevaRT <rt.shepeleva@gmail.com>
2024-09-12 16:52:37 +0300

    Сохранение в формате JSON для контактов

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactCreationTest.java
@@ -1,76 +1,70 @@
 package ru.stqa.addressbook.tests;
 
-import org.junit.jupiter.api.Test;
-import ru.stqa.addressbook.common.CommonFunctions;
-import ru.stqa.addressbook.model.ContactData;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import ru.stqa.addressbook.common.CommonFunctions;
+import ru.stqa.addressbook.model.ContactData;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTest extends TestBase {
 
-    public static List<ContactData> contactProvider() {
+    public static List<ContactData> contactProvider() throws IOException {
         var result = new ArrayList<ContactData>();
 
-        for (var firstname : List.of("", "firstname")) {
-            for (var lastname : List.of("", "lastname")) {
-                result.add(new ContactData()
-                        .withFirstname(firstname)
-                        .withMiddlename("")
-                        .withLastname(lastname)
-                        .withNickname("")
-                        .withPhoto("src/test/resources/images/avatar.png")
-                        .withTitle("")
-                        .withCompany("")
-                        .withAddress("")
-                        .withHome("")
-                        .withEmail("")
-                        .withHomepage("")
-                        .withBday("-")
-                        .withBmonth("-")
-                        .withByear("")
-                        .withAday("-")
-                        .withAmonth("-")
-                        .withAyear(""));
-            }
-        }
-        for (int i = 0; i < 5; i++) {
-            result.add(new ContactData()
-                    .withFirstname(CommonFunctions.randomString(i * 5))
-                    .withMiddlename("")
-                    .withLastname(CommonFunctions.randomString(i * 5))
-                    .withNickname("")
-                    .withPhoto("src/test/resources/images/avatar.png")
-                    .withTitle("")
-                    .withCompany("")
-                    .withAddress("")
-                    .withHome("")
-                    .withEmail("")
-                    .withHomepage("")
-                    .withBday("-")
-                    .withBmonth("-")
-                    .withByear("")
-                    .withAday("-")
-                    .withAmonth("-")
-                    .withAyear(""));
-//                    .withNickname(randomString(i * 5))
-//                    .withTitle(randomString(i * 5))
-//                    .withCompany(randomString(i * 5))
-//                    .withAddress(randomString(i * 5))
-//                    .withHome(randomString(i * 5))
-//                    .withEmail(randomString(i * 5))
-//                    .withHomepage(randomString(i * 5))
-//                    .withBday(randomIntDay())
-//                    .withBmonth(randomIntMonth())
-//                    .withByear(randomIntYear())
-//                    .withAday(randomIntDay())
-//                    .withAmonth(randomIntMonth())
-//                    .withAyear(randomIntYear())
-        }
+//        for (var firstname : List.of("", "firstname")) {
+//            for (var lastname : List.of("", "lastname")) {
+//                result.add(new ContactData()
+//                        .withFirstname(firstname)
+//                        .withMiddlename("")
+//                        .withLastname(lastname)
+//                        .withNickname("")
+//                        .withPhoto("src/test/resources/images/avatar.png")
+//                        .withTitle("")
+//                        .withCompany("")
+//                        .withAddress("")
+//                        .withHome("")
+//                        .withEmail("")
+//                        .withHomepage("")
+//                        .withBday("-")
+//                        .withBmonth("-")
+//                        .withByear("")
+//                        .withAday("-")
+//                        .withAmonth("-")
+//                        .withAyear(""));
+//            }
+//        }
+        ObjectMapper mapper = new ObjectMapper(); // create once, reuse
+        var value = mapper.readValue(new File("contacts.json"), new TypeReference<List<ContactData>>() {} );
+        result.addAll(value);
+//        for (int i = 0; i < 5; i++) {
+//            result.add(new ContactData()
+//                    .withFirstname(CommonFunctions.randomString(i * 5))
+//                    .withMiddlename("")
+//                    .withLastname(CommonFunctions.randomString(i * 5))
+//                    .withNickname("")
+//                    .withPhoto("src/test/resources/images/avatar.png")
+//                    .withTitle("")
+//                    .withCompany("")
+//                    .withAddress("")
+//                    .withHome("")
+//                    .withEmail("")
+//                    .withHomepage("")
+//                    .withBday("-")
+//                    .withBmonth("-")
+//                    .withByear("")
+//                    .withAday("-")
+//                    .withAmonth("-")
+//                    .withAyear(""));
+//        }
         return result;
     }
 
