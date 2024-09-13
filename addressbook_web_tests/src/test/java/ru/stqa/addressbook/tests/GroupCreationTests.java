4f6b78b94cec5690248d0aaaefb4dadb5a6832da
ShepelevaRT <rt.shepeleva@gmail.com>
2024-09-13 09:58:03 +0300

    Лекция 5.5. Подробнее про запись в файл и чтение из файла

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/GroupCreationTests.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.stqa.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -27,8 +31,20 @@ public class GroupCreationTests extends TestBase {
 //                }
 //            }
 //        }
+        var json = "";
+        try (var reader = new FileReader("groups.json");
+        var breader = new BufferedReader(reader)) {
+            var line = breader.readLine();
+            while (line != null) {
+                json = json + line;
+                line = breader.readLine();
+            }
+        } //файлы бывают очень большими, если можно читать файл строчка за строчкой, тогда все остальные просто игнорируются
+
+    //    var json = Files.readString(Paths.get("groups.json")); //если нужно прочитать сразу весь файл
         ObjectMapper mapper = new ObjectMapper(); // create once, reuse
-        var value = mapper.readValue(new File("groups.json"), new TypeReference<List<GroupData>>() {} );
+
+        var value = mapper.readValue(json, new TypeReference<List<GroupData>>() {} );
         result.addAll(value);
 //        for (int i = 0; i < 5; i++) {
 //            result.add(new GroupData()
